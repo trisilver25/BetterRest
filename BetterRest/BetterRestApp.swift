0d33620a6626e117c219fbb5e8046132ee327bcf
Tristin Smith <tsmithassoc@gmail.com>
2024-06-07 18:32:56 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tristin Smith on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
